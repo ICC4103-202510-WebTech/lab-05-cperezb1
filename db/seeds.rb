71d16204bdb86f0132236f0202d9b03b1aa85e0e
crist <crist@LAPTOP-O3VR4EOP.>
2025-04-12 20:48:58 -0400

    Lab 5 final

---
+++ db/seeds.rb
@@ -0,0 +1,34 @@
+#  10 usuarios con nombre y correo
+10.times do |i|
+    User.create!(
+      email: "user#{i}@example.com",                
+      first_name: "Nombre#{i}",                   
+      last_name: "Apellido#{i}"                     
+    )
+  end
+  
+  
+  10.times do
+    sender_id = User.pluck(:id).sample
+    receiver_id = User.pluck(:id).sample
+  
+    
+    while sender_id == receiver_id
+      receiver_id = User.pluck(:id).sample
+    end
+  
+    Chat.create!(
+      sender_id: sender_id,
+      receiver_id: receiver_id
+    )
+  end
+  
+  
+  10.times do
+    Message.create!(
+      chat_id: Chat.pluck(:id).sample,              
+      user_id: User.pluck(:id).sample,              
+      body: "Mensaje de prueba"
+    )
+  end
+  
